2bd07f5566c44e67266d2b8efc11db86c68eecf6
Alberto portillo <digital30@inter.mx>
2023-02-08 23:44:36 -0600

    change for today

--- README.md
+++ README.md
@@ -44,3 +44,6 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## My last Commet
+If you wanna do the same thing, check [Tutorial to create react app into github pages](https://github.com/gitname/react-gh-pages).
--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  async function click() {
+    if (window.confirm('Really you thing that?'))
+    {
+      window.location.replace('https://www.linkedin.com/in/israel-ledezma/')
+    }
+    else
+    {
+        alert('Maybe you are David!!!')
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p onClick={click}>
+          CLick on me if David is Stupid!!.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
